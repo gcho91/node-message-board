fd9dcbdee2dc896e1ac000a44d42c9465f8a67d2
Gloria Cho <gcho91@gmail.com>
2024-09-23 16:24:23 -0500

    Modify connectionstring for render

--- db/pool.js
+++ db/pool.js
@@ -2,10 +2,27 @@ const { Pool } = require("pg");
 const dotenv = require("dotenv");
 dotenv.config();
 
-module.exports = new Pool({
+const pool = new Pool({
   host: process.env.HOST,
   user: process.env.USER,
   database: process.env.DATABASE,
   password: process.env.SECRET,
   port: process.env.PORT,
+  connectionString: process.env.DATABASE_URL, // This is for Render
+
+  ssl: {
+    rejectUnauthorized: false, // Required for connecting to managed databases over SSL
+  },
 });
+
+// Test connection to the database
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error("Error acquiring client", err.stack);
+  } else {
+    console.log("Connected to the PostgreSQL database successfully");
+    release(); // Release the client back to the pool
+  }
+});
+
+module.exports = pool;
